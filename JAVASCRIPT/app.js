12380e131394a864b7fb46d187bf3ec3ca3a80bf
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-22 22:25:25 +0200

    adding shop and its functionality

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,7 +1,8 @@
 var pepes = 0;
+var gpps = 0; // Global pepes per second
 
 function mainButtonClick(){ 
- pepes++;
+ pepes += 1;
 }
 
 function updateCookies(){
@@ -9,4 +10,21 @@ function updateCookies(){
     pepesCounter.innerHTML = 'You have ' + pepes + ' Pepes';
 }
 
-// setInterval(updateCookies, 1);
+var item1 = {
+    name: 'Feels Bad Man',
+    price: 10,
+    pps: 1,
+    amount: 0,
+    buy: function(){
+        if(pepes >= this.price){
+            pepes -= this.price;
+            this.amount++;
+            gpps += this.pps;
+
+        }
+    }
+
+}
+
+setInterval(updateCookies, 1);
+setInterval(function(){ pepes += gpps; }, 1000);
