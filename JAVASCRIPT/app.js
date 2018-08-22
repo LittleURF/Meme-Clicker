877d58761e77177e879888268fdbcf91490a10da
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-22 10:58:44 +0200

    added button click functionality

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,1 +1,12 @@
-console.log('test');
+var pepes = 0;
+
+function mainButtonClick(){ 
+ pepes++;
+}
+
+function updateCookies(){
+    var pepesCounter = document.getElementById('pepe-counter');
+    pepesCounter.innerHTML = 'You have ' + pepes + ' Pepes';
+}
+
+setInterval(updateCookies, 1);
