26cc6a2ab54d34fe5a9bd7a09bd39b3c2d7f4143
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-24 11:32:56 +0200

    Made items into an array of objects + more rogue decimal stuff

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -4,6 +4,7 @@
 - Add upgrades functionality, onclick function with a paremeter of effectivness %
 - Upgrades will be split into categories, single item effectivness,
   all items effectivness, click power?
+- Make items into array of objects
 - Add statistics section
 - Make Buildings you dont have money for, stand out (low brightness fe.)
 - Add more buildings
@@ -21,10 +22,19 @@ function mainButtonClick() {
   pepes += 1;
 }
 
-function updateCookiesAndGpps() {
+function updateCookiesAndGppsUi() {
   var pepesCounter = document.getElementById('pepe-counter');
-  pepesCounter.innerHTML = 'You have ' + Math.round(pepes * 100) / 100 + ' Pepes';
+  pepesCounter.innerHTML = 'You have ' + Math.round(pepes) + ' Pepes';
   document.getElementById('gpps-counter').innerHTML = gpps + ' PPS'; 
+}
+
+function updateGpps(){
+
+}
+
+function roundNumberTo2(number) { // rounding number up to 2 decimal places to avoid rogue decimals
+  return number = Math.round(number * 100) / 100;
+
 }
 
 function buyItem(event, self) {
@@ -32,9 +42,9 @@ function buyItem(event, self) {
   if (pepes >= self.price) {
     pepes -= self.price;
     self.amount++;
-    self.pps = Math.round(self.pps * 100) / 100; // rounding pps up to 2 decimal places
+    self.pps = roundNumberTo2(self.pps); 
     gpps += self.pps;
-    gpps = Math.round(gpps * 100) / 100;
+    gpps = Math.round(gpps);
     self.price = Math.floor(self.firstPrice * Math.pow(1.2, self.amount))
     self.totalPps = self.amount * self.pps; // updates items total pps
     event.target.querySelector('span.shop-item-amount').innerHTML = self.amount; // updates amount on UI
@@ -44,42 +54,44 @@ function buyItem(event, self) {
 
 function upgradeItemEffectivness(item, effectivness){
   item.pps += Math.round((item.pps * (effectivness / 100)) * 100) / 100; 
-  item.pps = Math.round(item.pps * 100) / 100;
+  item.pps = roundNumberTo2(item.pps);
   // ^ Adds effectivness percentages to the number and rounds it to 2 decimal places
   console.log(item.pps);
 }
 
+var items = [
 
-var item1 = {
-  name: 'Feels Bad Man',
-  firstPrice: 10, // price of the first bought item
-  price: 10,
-  pps: 1,
-  amount: 0,
-  totalPps: 0
-};
+  item1 = {
+    name: 'Feels Bad Man',
+    firstPrice: 10, // price of the first bought item
+    price: 10,
+    pps: 1,
+    amount: 0,
+    totalPps: 0
+  },
 
-var item2 = {
-  name: 'Feels Good Man',
-  firstPrice: 250,
-  price: 250,
-  pps: 10,
-  amount: 0,
-  totalPps: 0
-};
+  item2 = {
+    name: 'Feels Good Man',
+    firstPrice: 250,
+    price: 250,
+    pps: 10,
+    amount: 0,
+    totalPps: 0
+  },
 
-var item3 = {
-  name: 'Feels Amazing Man',
-  firstPrice: 2000,
-  price: 2000,
-  pps: 100,
-  amount: 0,
-  totalPps: 0
-};
+  item3 = {
+    name: 'Feels Amazing Man',
+    firstPrice: 2000,
+    price: 2000,
+    pps: 100,
+    amount: 0,
+    totalPps: 0
+  }
+]
 
 
-
-setInterval(updateCookiesAndGpps, 1);
+setInterval(updateCookiesAndGppsUi, 1);
 setInterval(function () {
-  pepes += gpps;
-}, 1000);
+  pepes += gpps / 25;
+}, 40); // it actually adds 1 second worth of pepes every second, the timings are weird to make
+// the counter look cool
