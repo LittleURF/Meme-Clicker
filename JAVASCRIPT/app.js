a59ed58c1351b720273890519d1762f3816b4ae2
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-24 11:12:12 +0200

    start of upgrade functionality, dealing with rogue decimals

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,12 +1,17 @@
 /*
 --TO DO
 
-- Add upgrades section
+- Add upgrades functionality, onclick function with a paremeter of effectivness %
+- Upgrades will be split into categories, single item effectivness,
+  all items effectivness, click power?
 - Add statistics section
-- Make Buildings, you dont have money for, stand out (low brightness fe.)
+- Make Buildings you dont have money for, stand out (low brightness fe.)
 - Add more buildings
 - Think of something for Paula to draw
+- Save, Load mechanic
 - Replace upgrade icons with actual buildings they upgrade (modified pics tho)
+- Make upgrades dissapear when player is far away from them(no item,
+  earlier upgrade for this item is not yet bought)
 */
 
 var pepes = 100000;
@@ -16,9 +21,10 @@ function mainButtonClick() {
   pepes += 1;
 }
 
-function updateCookies() {
+function updateCookiesAndGpps() {
   var pepesCounter = document.getElementById('pepe-counter');
-  pepesCounter.innerHTML = 'You have ' + pepes + ' Pepes';
+  pepesCounter.innerHTML = 'You have ' + Math.round(pepes * 100) / 100 + ' Pepes';
+  document.getElementById('gpps-counter').innerHTML = gpps + ' PPS'; 
 }
 
 function buyItem(event, self) {
@@ -26,14 +32,21 @@ function buyItem(event, self) {
   if (pepes >= self.price) {
     pepes -= self.price;
     self.amount++;
+    self.pps = Math.round(self.pps * 100) / 100; // rounding pps up to 2 decimal places
     gpps += self.pps;
+    gpps = Math.round(gpps * 100) / 100;
     self.price = Math.floor(self.firstPrice * Math.pow(1.2, self.amount))
     self.totalPps = self.amount * self.pps; // updates items total pps
     event.target.querySelector('span.shop-item-amount').innerHTML = self.amount; // updates amount on UI
     event.target.querySelector('span.shop-item-price').innerHTML = self.price; // updates price on UI
-    document.getElementById('gpps-counter').innerHTML = gpps + ' PPS'; // updates gpps on UI
-
   }
+}
+
+function upgradeItemEffectivness(item, effectivness){
+  item.pps += Math.round((item.pps * (effectivness / 100)) * 100) / 100; 
+  item.pps = Math.round(item.pps * 100) / 100;
+  // ^ Adds effectivness percentages to the number and rounds it to 2 decimal places
+  console.log(item.pps);
 }
 
 
@@ -66,7 +79,7 @@ var item3 = {
 
 
 
-setInterval(updateCookies, 1);
+setInterval(updateCookiesAndGpps, 1);
 setInterval(function () {
   pepes += gpps;
 }, 1000);
