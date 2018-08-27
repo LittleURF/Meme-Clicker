407e884ca0310fa58201d46c48c10c96c01c6af6
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-27 15:43:40 +0200

    Added items and partly upgrades

--- JAVASCRIPT/ItemsAndUpgrades.js
+++ JAVASCRIPT/ItemsAndUpgrades.js
@@ -99,92 +99,198 @@ var items = [
       amount: 0,
       totalPps: 0
     },
+
+    item10 = {
+      id: 'item10',
+      name: 'Gachi',
+      firstPrice: 7700000000,
+      price: 7700000000,
+      pps: 51000000,
+      amount: 0,
+      totalPps: 0
+    },
+
+    item11 = {
+      id: 'item11',
+      name: 'Grey Face',
+      firstPrice: 85500000000,
+      price: 85500000000,
+      pps: 231400000,
+      amount: 0,
+      totalPps: 0
+    },
 ]
 
 var upgrades = [
   
   upgrade0 =  {
     upgradedItem: item0,
-    effectivness: 25,
+    effectivness: 50,
     price: 250,
     bought: false,
   },
   upgrade1 =  {
     upgradedItem: item0,
-    effectivness: 50,
+    effectivness: 100,
     price: 1500,
     bought: false,
   },
 
   upgrade2 =  {
-    upgradedItem: item0,
-    effectivness: 100,
+    upgradedItem: item1,
+    effectivness: 50,
     price: 10000,
     bought: false,
   },
 
   upgrade3 =  {
-    upgradedItem: item0,
+    upgradedItem: item1,
     effectivness: 100,
     price: 100000,
     bought: false,
   },
 
   upgrade4 =  {
-    upgradedItem: item1,
-    effectivness: 25,
+    upgradedItem: item2,
+    effectivness: 50,
     price: 2500,
     bought: false,
   },
 
   upgrade5 =  {
-    upgradedItem: item1,
-    effectivness: 50,
+    upgradedItem: item2,
+    effectivness: 100,
     price: 10000,
     bought: false,
   },
 
   upgrade6 =  {
-    upgradedItem: item1,
-    effectivness: 100,
+    upgradedItem: item3,
+    effectivness: 50,
     price: 55000,
     bought: false,
   },
 
   upgrade7 =  {
-    upgradedItem: item1,
+    upgradedItem: item3,
     effectivness: 100,
     price: 200000,
     bought: false,
   },
 
   upgrade8 =  {
-    upgradedItem: item2,
-    effectivness: 25,
+    upgradedItem: item4,
+    effectivness: 50,
     price: 15000,
     bought: false,
   },
 
   upgrade9 =  {
-    upgradedItem: item2,
-    effectivness: 50,
+    upgradedItem: item4,
+    effectivness: 100,
     price: 100500,
     bought: false,
   },
 
   upgrade10 =  {
-    upgradedItem: item2,
-    effectivness: 100,
+    upgradedItem: item5,
+    effectivness: 50,
     price: 2000000,
     bought: false,
   },
 
   upgrade11 =  {
-    upgradedItem: item2,
+    upgradedItem: item5,
     effectivness: 100,
     price: 50000000,
     bought: false,
   }
+  
+  upgrade12 =  {
+    upgradedItem: item6,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade13 =  {
+    upgradedItem: item6,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade14 =  {
+    upgradedItem: item7,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade15 =  {
+    upgradedItem: item7,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade16 =  {
+    upgradedItem: item8,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade17 =  {
+    upgradedItem: item8,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade18 =  {
+    upgradedItem: item9,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade19 =  {
+    upgradedItem: item9,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade20 =  {
+    upgradedItem: item10,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade21 =  {
+    upgradedItem: item10,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade22 =  {
+    upgradedItem: item11,
+    effectivness: 50,
+    price: 50000000,
+    bought: false,
+  }
+
+  upgrade23 =  {
+    upgradedItem: item11,
+    effectivness: 100,
+    price: 50000000,
+    bought: false,
+  }
+
+  
 ];
 
 var itemDOM =  document.getElementsByClassName('shop-item'); // array of every item UI element
