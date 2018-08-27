c68f8b4c329094b14f90dea893ff9b3bf1739a3e
Hubert Zachłowski <hubertzachlowski@gmail.com>
2018-08-27 22:55:41 +0200

    organizing code

--- JAVASCRIPT/ItemsAndUpgrades.js
+++ JAVASCRIPT/ItemsAndUpgrades.js
@@ -39,259 +39,259 @@ var items = [
     amount: 0,
     totalPps: 0
   },
-  
+
   item4 = {
-      id: 'item4',
-      name: 'Kermit',
-      firstPrice: 300000,
-      price: 300000,
-      pps: 5500,
-      amount: 0,
-      totalPps: 0
-    },
-  
-    item5 = {
-      id: 'item5',
-      name: 'Spongebob',
-      firstPrice: 1200000,
-      price: 1200000,
-      pps: 23000,
-      amount: 0,
-      totalPps: 0
-    },
-  
-    item6 = {
-      id: 'item6',
-      name: 'Dat Boi',
-      firstPrice: 6500000,
-      price: 6500000,
-      pps: 113000,
-      amount: 0,
-      totalPps: 0
-    },
+    id: 'item4',
+    name: 'Kermit',
+    firstPrice: 300000,
+    price: 300000,
+    pps: 5500,
+    amount: 0,
+    totalPps: 0
+  },
 
-    item7 = {
-      id: 'item7',
-      name: 'Spooky Scary Skeleton',
-      firstPrice: 36000000,
-      price: 36000000,
-      pps: 550000,
-      amount: 0,
-      totalPps: 0
-    },
+  item5 = {
+    id: 'item5',
+    name: 'Spongebob',
+    firstPrice: 1200000,
+    price: 1200000,
+    pps: 23000,
+    amount: 0,
+    totalPps: 0
+  },
 
-    item8 = {
-      id: 'item8',
-      name: 'Distracted Boyfriend',
-      firstPrice: 215000000,
-      price: 215000000,
-      pps: 2400000,
-      amount: 0,
-      totalPps: 0
-    },
+  item6 = {
+    id: 'item6',
+    name: 'Dat Boi',
+    firstPrice: 6500000,
+    price: 6500000,
+    pps: 113000,
+    amount: 0,
+    totalPps: 0
+  },
 
-    item9 = {
-      id: 'item9',
-      name: 'Feels Amazing Man',
-      firstPrice: 1200000000,
-      price: 1200000000,
-      pps: 11400000,
-      amount: 0,
-      totalPps: 0
-    },
+  item7 = {
+    id: 'item7',
+    name: 'Spooky Scary Skeleton',
+    firstPrice: 36000000,
+    price: 36000000,
+    pps: 550000,
+    amount: 0,
+    totalPps: 0
+  },
 
-    item10 = {
-      id: 'item10',
-      name: 'Gachi',
-      firstPrice: 7700000000,
-      price: 7700000000,
-      pps: 51000000,
-      amount: 0,
-      totalPps: 0
-    },
+  item8 = {
+    id: 'item8',
+    name: 'Distracted Boyfriend',
+    firstPrice: 215000000,
+    price: 215000000,
+    pps: 2400000,
+    amount: 0,
+    totalPps: 0
+  },
 
-    item11 = {
-      id: 'item11',
-      name: 'Grey Face',
-      firstPrice: 85500000000,
-      price: 85500000000,
-      pps: 231400000,
-      amount: 0,
-      totalPps: 0
-    },
+  item9 = {
+    id: 'item9',
+    name: 'Feels Amazing Man',
+    firstPrice: 1200000000,
+    price: 1200000000,
+    pps: 11400000,
+    amount: 0,
+    totalPps: 0
+  },
+
+  item10 = {
+    id: 'item10',
+    name: 'Gachi',
+    firstPrice: 7700000000,
+    price: 7700000000,
+    pps: 51000000,
+    amount: 0,
+    totalPps: 0
+  },
+
+  item11 = {
+    id: 'item11',
+    name: 'Grey Face',
+    firstPrice: 85500000000,
+    price: 85500000000,
+    pps: 231400000,
+    amount: 0,
+    totalPps: 0
+  },
 ]
 
 var upgrades = [
-  
-  upgrade0 =  {
+
+  upgrade0 = {
     upgradedItem: item0,
     effectivness: 50,
     price: 250,
     bought: false,
   },
-  upgrade1 =  {
+  upgrade1 = {
     upgradedItem: item0,
     effectivness: 100,
     price: 1500,
     bought: false,
   },
 
-  upgrade2 =  {
+  upgrade2 = {
     upgradedItem: item1,
     effectivness: 50,
     price: 10000,
     bought: false,
   },
 
-  upgrade3 =  {
+  upgrade3 = {
     upgradedItem: item1,
     effectivness: 100,
     price: 100000,
     bought: false,
   },
 
-  upgrade4 =  {
+  upgrade4 = {
     upgradedItem: item2,
     effectivness: 50,
     price: 2500,
     bought: false,
   },
 
-  upgrade5 =  {
+  upgrade5 = {
     upgradedItem: item2,
     effectivness: 100,
     price: 10000,
     bought: false,
   },
 
-  upgrade6 =  {
+  upgrade6 = {
     upgradedItem: item3,
     effectivness: 50,
     price: 55000,
     bought: false,
   },
 
-  upgrade7 =  {
+  upgrade7 = {
     upgradedItem: item3,
     effectivness: 100,
     price: 200000,
     bought: false,
   },
 
-  upgrade8 =  {
+  upgrade8 = {
     upgradedItem: item4,
     effectivness: 50,
     price: 15000,
     bought: false,
   },
 
-  upgrade9 =  {
+  upgrade9 = {
     upgradedItem: item4,
     effectivness: 100,
     price: 100500,
     bought: false,
   },
 
-  upgrade10 =  {
+  upgrade10 = {
     upgradedItem: item5,
     effectivness: 50,
     price: 2000000,
     bought: false,
   },
 
-  upgrade11 =  {
+  upgrade11 = {
     upgradedItem: item5,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
-  
-  upgrade12 =  {
+
+  upgrade12 = {
     upgradedItem: item6,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade13 =  {
+  upgrade13 = {
     upgradedItem: item6,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  upgrade14 =  {
+  upgrade14 = {
     upgradedItem: item7,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade15 =  {
+  upgrade15 = {
     upgradedItem: item7,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  upgrade16 =  {
+  upgrade16 = {
     upgradedItem: item8,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade17 =  {
+  upgrade17 = {
     upgradedItem: item8,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  upgrade18 =  {
+  upgrade18 = {
     upgradedItem: item9,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade19 =  {
+  upgrade19 = {
     upgradedItem: item9,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  upgrade20 =  {
+  upgrade20 = {
     upgradedItem: item10,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade21 =  {
+  upgrade21 = {
     upgradedItem: item10,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  upgrade22 =  {
+  upgrade22 = {
     upgradedItem: item11,
     effectivness: 50,
     price: 50000000,
     bought: false,
   },
 
-  upgrade23 =  {
+  upgrade23 = {
     upgradedItem: item11,
     effectivness: 100,
     price: 50000000,
     bought: false,
   },
 
-  
+
 ];
 
-var itemDOM =  document.getElementsByClassName('shop-item'); // array of every item UI element
+var itemDOM = document.getElementsByClassName('shop-item'); // array of every item UI element
 var upgradesDOM = document.getElementsByClassName('upgrade-image'); // array of every upgrade UI element
--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,9 +1,8 @@
 /*
 --TO DO
-Right now - Add items and change upgrades to have 2 for each item, then make the actual images for them
+ upgrades make the actual images for them
 Trebuchet? Star wars? Drake? Kappa, GachiGASM/Van Darkholm
 
-- Add more buildings
 - Think of something for Paula to draw
 - Change upgrades prices in html + js and more stuff in html
 - Replace upgrade icons with actual buildings they upgrade (modified pics tho)
@@ -12,7 +11,6 @@ Trebuchet? Star wars? Drake? Kappa, GachiGASM/Van Darkholm
   earlier upgrade for this item is not yet bought)
 - Add other upgrade types, all items effectivness, click effectivness
 - Add statistics section
-- Maybe get the rounding of numbers in order? Its all over the place as of now.
 - Make buildings dissaper if player doesnt have the previous one(?) and money for it(?)
 
 - Sometimes all upgrades dissapear when you buy one of them, check after beign done with making upgrades
@@ -26,202 +24,193 @@ Trebuchet? Star wars? Drake? Kappa, GachiGASM/Van Darkholm
 - Make Circle Game pic say - Made you look! at the bottom or somewhere
 
 */
-(function(){
-
-  
-var pepes = 4500000000;
-var gpps = 0; // Global pepes per second
+(function () {
 
 
-// if(localStorage.getItem("save") !== null){ // If a previous save exists, load it.
-//   console.log('loading');
-//   loadGame();
-// }
+  var pepes = 4500000000;
+  var gpps = 0; // Global pepes per second
 
 
-function saveGame(){
-  var save = {
-    pepes: pepes,
-    gpps: gpps,
-    items: items,
-    upgrades: upgrades,
-  };
-  localStorage.setItem("save", JSON.stringify(save));
-}
+  // if(localStorage.getItem("save") !== null){ // If a previous save exists, load it.
+  //   console.log('loading');
+  //   loadGame();
+  // }
 
-function loadGame(){
-  var save = JSON.parse(localStorage.getItem("save"));
-  pepes = save.pepes;
-  gpps = save.gpps;
-  items = save.items;
-  upgrades = save.upgrades;
-  updateUi();
-}
 
-function updateUi(){ // Updates UI with loaded(from a save) things
-  for (var i = 0; i < items.length; i++) {
-    document.getElementById(items[i].id).querySelector('span.shop-item-amount').innerHTML = convertBigNumber(items[i].amount);
-    document.getElementById(items[i].id).querySelector('span.shop-item-pps').innerHTML = 'PPS: ' + convertBigNumber(items[i].pps);
-    document.getElementById(items[i].id).querySelector('span.shop-item-price').innerHTML = convertBigNumber(items[i].price);
+  function saveGame() {
+    var save = {
+      pepes: pepes,
+      gpps: gpps,
+      items: items,
+      upgrades: upgrades,
+    };
+    localStorage.setItem("save", JSON.stringify(save));
+  }
+
+  function loadGame() {
+    var save = JSON.parse(localStorage.getItem("save"));
+    pepes = save.pepes;
+    gpps = save.gpps;
+    items = save.items;
+    upgrades = save.upgrades;
+    updateUi();
+  }
+
+  function updateUi() { // Updates UI with loaded(from a save) things
+    for (var i = 0; i < items.length; i++) {
+      document.getElementById(items[i].id).querySelector('span.shop-item-amount').innerHTML = convertBigNumber(items[i].amount);
+      document.getElementById(items[i].id).querySelector('span.shop-item-pps').innerHTML = 'PPS: ' + convertBigNumber(items[i].pps);
+      document.getElementById(items[i].id).querySelector('span.shop-item-price').innerHTML = convertBigNumber(items[i].price);
     }
+
+    for (var j = 0; j < upgrades.length; j++) {
+      if (upgrades[j].bought === true)
+        document.getElementById('upgrade' + j).style.display = 'none';
+    }
+  }
+
+  var mainPepeButton = document.getElementById('main-pepe-button');
+  mainPepeButton.addEventListener("click", mainButtonClick, false);
+
+
+  function mainButtonClick() {
+
+    pepes += 1;
+  }
+
+
+
+  // function normalizeNumbers(number) {
+  //   number = number.replace(/\s/g, '');
+  //   return number;
+  // }
+
+  function convertBigNumber(number) {
+
+    switch (true) {
+      case (number >= 1000000000000):
+        number = (number / 1000000000000).toFixed(3) + ' T';
+        break;
+      case (number >= 1000000000):
+        number = (number / 1000000000).toFixed(3) + ' B';
+        break;
+      case (number >= 1000000):
+        number = (number / 1000000).toFixed(3) + ' M';
+        break;
+      case (number >= 1000):
+        number = (number / 1000).toFixed(3) + ' K';
+        break;
+    }
+    return number;
+  }
+
+  function updateCookiesAndGppsUi() {
+    var pepeCounter = document.getElementById('pepe-counter');
+    pepeCounter.innerHTML = convertBigNumber(Math.round(pepes)) + ' Pepes';
+    document.getElementById('gpps-counter').innerHTML = convertBigNumber(gpps) + ' PPS';
+    // console.log(pepes);
+
+    for (var i = 0; i < items.length; i++) { // goes through every item and lights it up if player has enough pepes to buy it
+      if (pepes >= items[i].price) {
+        document.getElementById(items[i].id).style.opacity = '1';
+        document.getElementById(items[i].id).style.filter = 'none';
+      } else {
+        document.getElementById(items[i].id).style.opacity = '0.45';
+        document.getElementById(items[i].id).style.filter = 'grayscale(100%)';
+      }
+      if (items[i].amount == 0) // If item is not bought, pps is greyed out
+        document.getElementById(items[i].id).querySelector('span.shop-item-pps').style.opacity = '0.4';
+      else
+        document.getElementById(items[i].id).querySelector('span.shop-item-pps').style.opacity = '1';
+    }
+
+
+    for (var j = 0; j < upgradesDOM.length; j++) { // goes through every upgrade and lights it up if player has enough pepes to buy it
+      var upgradePrice = upgradesDOM[j].querySelector('span.upgrade-price').innerHTML.replace(/\s/g, ''); // removes whitespace from the price
+      if (pepes < upgrades[j].price) {
+        upgradesDOM[j].style.opacity = '0.4';
+      } else {
+        upgradesDOM[j].style.opacity = '1';
+      }
+
+    }
+  }
+
+
+  function updateGpps() {
+    var toBeGpps = 0;
+    for (var i = 0; i < items.length; i++) {
+      toBeGpps += items[i].totalPps;
+    }
+    toBeGpps = Math.round(toBeGpps);
+    gpps = toBeGpps;
+    startOrRestartWorker();
+    // updateCookiesAndGppsUi(); // It was once here but i think its useless, in case everything breaks, check if this is the reason
+  }
+
+
+
+  function updateItemsPps(item) {
+    document.getElementById(item.id).querySelector('span.shop-item-pps').innerHTML = 'PPS: ' + convertBigNumber(item.totalPps);
+  }
+
+
+  function buyItem(item) {
+    if (pepes >= item.price) {
+      pepes -= item.price;
+      item.amount++;
+      item.price = Math.floor(item.firstPrice * Math.pow(1.2, item.amount))
+      item.totalPps = item.amount * item.pps; // updates items total pps
+      var itemDOM = document.getElementById(item.id)
+      itemDOM.querySelector('span.shop-item-amount').innerHTML = convertBigNumber(item.amount); // updates amount on UI
+      itemDOM.querySelector('span.shop-item-price').innerHTML = convertBigNumber(item.price); // updates price on UI
+      updateGpps();
+      updateItemsPps(item);
+    }
+  }
+
+  for (var i = 0; i < items.length; i++) {
+    itemDOM[i].addEventListener("click", buyItem.bind(this, items[i]), false);
+  }
 
   for (var j = 0; j < upgrades.length; j++) {
-    if (upgrades[j].bought === true)
-    document.getElementById('upgrade' + j).style.display = 'none';
+    upgradesDOM[j].addEventListener("click", upgradeItem.bind(this, upgrades[j]), false);
   }
-}
 
-var mainPepeButton = document.getElementById('main-pepe-button');
-mainPepeButton.addEventListener("click", mainButtonClick, false);
-
-
-function mainButtonClick() {
-
-  pepes += 1;
-}
-
-
-
-// function normalizeNumbers(number) {
-//   number = number.replace(/\s/g, '');
-//   return number;
-// }
-
-function convertBigNumber(number){
-
-  switch(true){
-    case (number >= 1000000000000):
-      number = (number / 1000000000000).toFixed(3) + ' T';
-      break;
-    case (number >= 1000000000):
-      number = (number / 1000000000).toFixed(3) + ' B';
-      break;
-    case (number >= 1000000):
-      number = (number / 1000000).toFixed(3) + ' M';
-      break;
-    case (number >= 1000):
-      number = (number / 1000).toFixed(3) + ' K';
-      break;
-  }
-  return number;
-}
-
-function updateCookiesAndGppsUi() {
-  var pepeCounter = document.getElementById('pepe-counter');
-  pepeCounter.innerHTML = convertBigNumber(Math.round(pepes)) + ' Pepes';
-  document.getElementById('gpps-counter').innerHTML = convertBigNumber(gpps) + ' PPS';
-  // console.log(pepes);
-
-  for (var i = 0; i < items.length; i++) { // goes through every item and lights it up if player has enough pepes to buy it
-    if (pepes >= items[i].price){
-      document.getElementById(items[i].id).style.opacity = '1';
-      document.getElementById(items[i].id).style.filter = 'none';
+  function upgradeItem(upgrade) {
+    if (pepes >= upgrade.price && upgrade.bought === false) {
+      pepes -= upgrade.price;
+      upgrade.bought = true;
+      upgrade.upgradedItem.pps += Math.round((upgrade.upgradedItem.pps * (upgrade.effectivness / 100)) * 100) / 100;
+      upgrade.upgradedItem.totalPps = upgrade.upgradedItem.amount * upgrade.upgradedItem.pps; // updates items total pps
+      updateGpps();
+      event.target.parentElement.style.display = 'none';
+      updateItemsPps(upgrade.upgradedItem);
     }
-    else {
-      document.getElementById(items[i].id).style.opacity = '0.45';
-      document.getElementById(items[i].id).style.filter = 'grayscale(100%)';
-    }
-    if ( items[i].amount == 0) // If item is not bought, pps is greyed out
-      document.getElementById(items[i].id).querySelector('span.shop-item-pps').style.opacity = '0.4';
-    else
-    document.getElementById(items[i].id).querySelector('span.shop-item-pps').style.opacity = '1';
   }
- 
-
-  for (var j = 0; j < upgradesDOM.length; j++){ // goes through every upgrade and lights it up if player has enough pepes to buy it
-    var upgradePrice = upgradesDOM[j].querySelector('span.upgrade-price').innerHTML.replace(/\s/g, ''); // removes whitespace from the price
-    if(pepes < upgrades[j].price){
-      upgradesDOM[j].style.opacity = '0.4';
-    }
-    else {
-      upgradesDOM[j].style.opacity = '1';
-    }
-  
-  }
-}
 
 
-function updateGpps() {
-  var toBeGpps = 0;
-  for (var i = 0; i < items.length; i++) {
-    toBeGpps += items[i].totalPps;
+  var pepeCounterWorker;
+
+  function startOrRestartWorker() {
+
+    if (pepeCounterWorker != undefined) { // If the worker is already made, it terminates it to make a new one, without it there are multiple workers all adding GPPS
+      pepeCounterWorker.terminate();
+    }
+    // This worker makes GPPS work even if the tab is not active
+    // this long boy down here? Dont ask, it has to do with chrome not beign able to load workers though.
+    pepeCounterWorker = new Worker(URL.createObjectURL(new Blob(["(" + worker_function.toString() + ")()"], {
+      type: 'text/javascript'
+    })));
+    pepeCounterWorker.onmessage = function (e) {
+      pepes += e.data;
+    };
+
+    pepeCounterWorker.postMessage(gpps);
   }
-  toBeGpps = Math.round(toBeGpps);
-  gpps = toBeGpps;
+  updateUi();
   startOrRestartWorker();
- // updateCookiesAndGppsUi(); // It was once here but i think its useless, in case everything breaks, check if this is the reason
-}
 
-
-
-function updateItemsPps(item) {
-  document.getElementById(item.id).querySelector('span.shop-item-pps').innerHTML = 'PPS: ' + convertBigNumber(item.totalPps);
-}
-
-
-function buyItem(item) {
-  if (pepes >= item.price) {
-    pepes -= item.price;
-    item.amount++;
-    item.price = Math.floor(item.firstPrice * Math.pow(1.2, item.amount))
-    item.totalPps =item.amount * item.pps; // updates items total pps
-    var itemDOM = document.getElementById(item.id)
-    itemDOM.querySelector('span.shop-item-amount').innerHTML = convertBigNumber(item.amount); // updates amount on UI
-    itemDOM.querySelector('span.shop-item-price').innerHTML = convertBigNumber(item.price); // updates price on UI
-    updateGpps();
-    updateItemsPps(item);
-  }
-}
-
-for (var i = 0; i < items.length; i++){
-  itemDOM[i].addEventListener("click", buyItem.bind(this, items[i]), false);
-}
-
-for (var j = 0; j < upgrades.length; j++){
-  upgradesDOM[j].addEventListener("click", upgradeItem.bind(this, upgrades[j]), false);
-}
-
-function upgradeItem(upgrade) {
-  if (pepes >= upgrade.price && upgrade.bought === false) {
-    pepes -= upgrade.price;
-    upgrade.bought = true;
-    upgrade.upgradedItem.pps += Math.round((upgrade.upgradedItem.pps * (upgrade.effectivness / 100)) * 100) / 100;
-    upgrade.upgradedItem.totalPps = upgrade.upgradedItem.amount * upgrade.upgradedItem.pps; // updates items total pps
-    updateGpps();
-    event.target.parentElement.style.display = 'none';
-    updateItemsPps(upgrade.upgradedItem);
-  }
-}
-
-
-var pepeCounterWorker;
-function startOrRestartWorker(){
-
-  if(pepeCounterWorker != undefined){ // If the worker is already made, it terminates it to make a new one, without it there are multiple workers all adding GPPS
-    pepeCounterWorker.terminate();
-  }
-// This worker makes GPPS work even if the tab is not active
-// this long boy down here? Dont ask, it has to do with chrome not beign able to load workers though.
-pepeCounterWorker = new Worker(URL.createObjectURL(new Blob(["("+worker_function.toString()+")()"], {type: 'text/javascript'}))); 
-pepeCounterWorker.onmessage =  function(e) {
-  pepes += e.data;
-};
-
-pepeCounterWorker.postMessage(gpps);
-}
-updateUi();
-startOrRestartWorker();
-
-
-
-
-
-
-
-
-
-
-
-setInterval(updateCookiesAndGppsUi, 10);
-setInterval(saveGame, 3000);
+  setInterval(updateCookiesAndGppsUi, 10);
+  setInterval(saveGame, 3000);
 })();
